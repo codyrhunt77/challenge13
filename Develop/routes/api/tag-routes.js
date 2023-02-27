c97ffae52ab983fea8a3534f0937cfde0856b204
Cody Hunt <codyrhunt77@gmail.com>
2023-02-26 20:32:28 -0500

    added functionality

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,25 +4,32 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+ Tag.findOne({ include: [{
+  model: Product,
+  through: ProductTag,
+ },],})
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findAll({ where: { id: req.params.id},
+  include: [{
+    model: Product,
+    through: ProductTag,
+  },],})
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.update(req.body, {
+    where: { id: req.params.id,},
+  })
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.create(req.body)
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+ Tag.destroy({ where: { id: req.params.id,},})
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sequelize = require('./config/connection.js')
 const routes = require('./routes');
 // import sequelize connection
 
